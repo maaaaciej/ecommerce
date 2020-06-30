ed443b614173fcd9fd99a83924dc370ad80d8afb
Maciej <ostromaciej@gmail.com>
2020-06-30 14:15:53 +0200

    added shop page, made components for each store item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "./menu-item.styles.scss";
-import {withRouter} from "react-router-dom"
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size, history,linkUrl, match}) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
     <div
-      onClick={()=>history.push(`${match.url}${linkUrl}`)}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
       className={`${size} menu-item`}
     >
-      <div className="background-image" style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}/>
-        <div className="content">
-          <h1 className="title">{title.toUpperCase()}</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-     </div>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./preview-collection.style.scss";
+import CollectionItem from "../collection-item/collection-item.component"
+
+const Preview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="preview">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(({id, ...itemProps}) => (
+            <CollectionItem key={id} {...itemProps}/>
+          ))}
+      </div>
+    </div>
+
+  );
+};
+
+export default Preview;
