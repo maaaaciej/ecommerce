08437c4451d549e2544942ff663d4cbc655e1382
Maciej <ostromaciej@gmail.com>
2020-07-03 10:22:45 +0200

    ayy, cart now displays items in the cartItems array in our state

--- src/components/shopping-cart/shopping-cart.components.jsx
+++ src/components/shopping-cart/shopping-cart.components.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import "./shopping-cart.style.scss"
-import CustomButton from "../custom-button/custom-button.components"
+import React from "react";
+import { connect } from "react-redux";
+import "./shopping-cart.style.scss";
+import CustomButton from "../custom-button/custom-button.components";
 
-const Cart = () => {
-    return ( <div className="cart-dropdown">
-        <div className="cart-items"/>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
-    </div> );
-}
- 
-export default Cart;
+import CartItem from "../cart-item/cart-item.component";
+
+const Cart = ({ cartItems }) => {
+  return (
+    <div className='cart-dropdown'>
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+  );
+};
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+export default connect(mapStateToProps)(Cart);
